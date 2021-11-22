b257d22d9276e652c66bb149f5cc4fafd8215c79
MaeyD <75167436+MaeyD@users.noreply.github.com>
2021-11-23 05:13:07 +0530

    Create notifier.py

---
+++ notifier.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+if __name__ == '__main__':
+ 	while True:
+ 		notification.notify(
+ 			title = "!! RELAX !!",
+ 			message ="~ Take a break ~ Straighten your posture ~ Relax your eyes ~ Drink some water ~",
+ 			timeout= 15
+ 			)
+ 		time.sleep(60*30)
